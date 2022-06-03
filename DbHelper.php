abcb082b8c2e2c2ad336d05dff85d53e3867e496
Frugone <frugone@gmail.com>
2022-06-03 19:23:29 +0200

    first commit

---
+++ DbHelper.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Helpers
+
+class BbHelper
+{
+    /**
+     * Combines SQL and its bindings
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @return string
+     */
+    public static function sqlfromBindings($query, $print = true)
+    {
+        $percentTemp = '^&^';
+        $sql = str_replace(['%', '?'], [$percentTemp, '%s'], $query->toSql());
+
+        $params = collect($query->getBindings())->map(
+            function ($binding) {
+                return is_numeric($binding) ? $binding : "'{$binding}'";
+            }
+        )->toArray();
+
+        $str = vsprintf($sql, $params);
+        $sql = str_replace($percentTemp, '%', $str);
+        if ($print) {
+            print($sql);
+            die;
+        } else {
+            return $sql;
+        }
+    }
+}
